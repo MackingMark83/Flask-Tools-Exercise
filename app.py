94575985fd71f297ba69610dd098c28798f0b991
Marcus <marcusclemmons1@gmail.com>
2021-12-03 10:40:41 -0500

    info

--- app.py
+++ app.py
@@ -1,3 +1,19 @@
-from flask import Flask 
+from flask import Flask,request, render_template, redirect, flash  
+from flask_debugtoolbar import DebugToolbarExtension
+from surveys import satisfaction_survey as survey
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "mysecret"
+debug = DebugToolbarExtension(app)
+
+
+responses = []
+
+
+
+@app.route("/")
+def show_survey_start():
+    """Select a survey."""
+
+    return render_template("survey_start.html", survey=survey)
