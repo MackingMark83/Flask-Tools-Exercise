be36f1163a878309ff48f3ae2b9270250d94db7b
Marcus <marcusclemmons1@gmail.com>
2021-12-04 11:13:14 -0500

    just about done

--- app.py
+++ app.py
@@ -1,19 +1,76 @@
-from flask import Flask,request, render_template, redirect, flash  
+from flask import Flask, request, render_template, redirect, flash
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
-
-app = Flask(__name__)
-
-app.config['SECRET_KEY'] = "mysecret"
-debug = DebugToolbarExtension(app)
-
 
 responses = []
 
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "my secert"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
+
 
 @app.route("/")
 def show_survey_start():
-    """Select a survey."""
+    """shows the begin page."""
 
     return render_template("survey_start.html", survey=survey)
+
+
+@app.route("/begin", methods=["POST"])
+def start_survey():
+    """Clear the session of responses."""
+
+    
+
+    return redirect("/questions/0")
+
+
+@app.route("/answer", methods=["POST"])
+def handle_question():
+    """Save response and redirect to next question."""
+
+    # get the response choice
+    choice = request.form['answer']
+
+   
+    responses.append(choice)
+    
+
+    if (len(responses) == len(survey.questions)):
+        # They've answered all the questions! Thank them.
+        return redirect("/complete")
+
+    else:
+        return redirect(f"/questions/{len(responses)}")
+
+
+@app.route("/questions/<int:qid>")
+def show_question(qid):
+    """Display current question."""
+   
+
+    if (responses is None):
+        # trying to access question page too soon
+        return redirect("/")
+
+    if (len(responses) == len(survey.questions)):
+        # They've answered all the questions! Thank them.
+        return redirect("/complete")
+
+    if (len(responses) != qid):
+        # Trying to access questions out of order.
+        flash(f"Invalid question id: {qid}.")
+        return redirect(f"/questions/{len(responses)}")
+
+    question = survey.questions[qid]
+    return render_template(
+        "question.html", question_num=qid, question=question)
+
+
+@app.route("/complete")
+def complete():
+    """Survey complete. Show completion page."""
+
+    return render_template("completion.html")
